1aa12d3bddcc04108b4528910ff08d2c5c11c148
AyaneSHOJI <shojiayane@gmail.com>
2022-06-17 12:23:26 +0200

    set up project

--- WorkingWithFileSystems/Program.cs
+++ WorkingWithFileSystems/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
