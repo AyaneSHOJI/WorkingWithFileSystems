aea041ad83d8d4eb9f7c2b79a74344e33335177f
AyaneSHOJI <shojiayane@gmail.com>
2022-06-17 12:43:53 +0200

    p.370 output files system info

--- WorkingWithFileSystems/Program.cs
+++ WorkingWithFileSystems/Program.cs
@@ -3,3 +3,21 @@ using static System.IO.Directory;
 using static System.IO.Path;
 using static System.Environment;
 
+static void OutputFileSystemInfo()
+{
+    WriteLine("{0,-33}{1}", arg0: "Path.PathSeparator", arg1: PathSeparator);
+    WriteLine("{0,-33}{1}", arg0: "Path.DirectorySeparatorChar", arg1: DirectorySeparatorChar);
+    WriteLine("{0,-33}{1}", arg0: "Directory.GetCurrentDirectory", arg1: Directory.GetCurrentDirectory);
+    WriteLine("{0,-33}{1}", arg0: "Environment.CurrentDirectory", arg1: Environment.CurrentDirectory);    
+    WriteLine("{0,-33}{1}", arg0: "Environment.SystemDirectory", arg1: Environment.SystemDirectory);
+    WriteLine("{0,-33}{1}", arg0: "Path.GetTempPath()", arg1: Path.GetTempPath());
+
+    WriteLine("GetFolderPath(SpecialFolder)");
+
+    WriteLine("{0,-33}{1}", arg0: " .System", arg1: GetFolderPath(SpecialFolder.System));
+    WriteLine("{0,-33}{1}", arg0: " .ApplicationData", arg1: GetFolderPath(SpecialFolder.ApplicationData));
+    WriteLine("{0,-33}{1}", arg0: " .MyDocuments", arg1: GetFolderPath(SpecialFolder.MyDocuments));
+    WriteLine("{0,-33}{1}", arg0: " .Personal", arg1: GetFolderPath(SpecialFolder.Personal));
+}
+
+OutputFileSystemInfo();
